e3e741efb74600ff7cdd011ec563215a09fcfb7d
Olena Voroshnina <yuriyilena67@gmail.com>
2023-03-26 21:40:06 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -5,37 +5,53 @@ const { nanoid } = require('nanoid');
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  return contacts;
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find(item => item.id === contactId);
-  if (!contact) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(item => item.id === contactId);
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
   }
-  return contact;
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const newContact = { id: nanoid(2), name, email, phone };
-  const contactsAll = [...contacts, newContact];
-  await fs.writeFile(contactsPath, JSON.stringify(contactsAll));
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(2), name, email, phone };
+    const contactsAll = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(contactsAll));
+    return newContact;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const idx = contacts.findIndex(item => item.id === contactId);
-  if (idx === -1) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => item.id === contactId);
+    if (idx === -1) {
+      return null;
+    }
+    const newContacts = contacts.filter((_, index) => index !== idx);
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return contacts[idx];
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
   }
-  const newContacts = contacts.filter((_, index) => index !== idx);
-  await fs.writeFile(contactsPath, JSON.stringify(newContacts));
-  return contacts[idx];
 }
 
 module.exports = {
